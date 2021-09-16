471dbb7090f64d1aa2db8bd9f8a8a4bc45aec31b
Mustafa <cana66331@gmail.com>
2021-09-16 22:25:06 +0200

    feat(footer): added footer component

--- src/utils/routes/index.ts
+++ src/utils/routes/index.ts
@@ -6,6 +6,12 @@ export type RouteLabelType =
   | 'news'
   | 'competition'
   | 'hashtag'
+  | 'helpCenter'
+  | 'termsOfService'
+  | 'legal'
+  | 'privacyPolicy'
+  | 'status'
+  | 'contactUs'
 
 type InternalLinksType = Record<
   RouteLabelType,
@@ -111,6 +117,90 @@ export const ROUTES: InternalLinksType = {
       link: '/etiket-etkinlikleri',
     },
   },
+  helpCenter: {
+    en: {
+      name: 'Help Center',
+      link: '/help-center',
+    },
+    nl: {
+      name: 'Helpcentrum',
+      link: '/help-centrum',
+    },
+    tr: {
+      name: 'Yardım Merkezi',
+      link: '/yardim-merkezi',
+    },
+  },
+  termsOfService: {
+    en: {
+      name: 'Terms of service',
+      link: '/terms-of-service',
+    },
+    nl: {
+      name: 'Aankondigingen',
+      link: '/aankondigingen',
+    },
+    tr: {
+      name: 'Kullanım Şartları',
+      link: '/kullanım-sartları',
+    },
+  },
+  legal: {
+    en: {
+      name: 'Legal',
+      link: '/legal',
+    },
+    nl: {
+      name: 'Legaal',
+      link: '/legaal',
+    },
+    tr: {
+      name: 'Yasal',
+      link: '/yasal',
+    },
+  },
+  privacyPolicy: {
+    en: {
+      name: 'Privacy Policy',
+      link: '/privacy-policy',
+    },
+    nl: {
+      name: 'Wedstrijden',
+      link: '/wedstrijden',
+    },
+    tr: {
+      name: 'Gizlilik Politikası',
+      link: '/gizlilik-politikası',
+    },
+  },
+  status: {
+    en: {
+      name: 'Status',
+      link: '/status',
+    },
+    nl: {
+      name: 'Toestand',
+      link: '/toestand',
+    },
+    tr: {
+      name: 'Durum',
+      link: '/durum',
+    },
+  },
+  contactUs: {
+    en: {
+      name: 'Contact Us',
+      link: '/contact-us',
+    },
+    nl: {
+      name: 'Neem contact op',
+      link: '/neem-contact-op',
+    },
+    tr: {
+      name: 'Bize Ulaşın',
+      link: '/bize-ulasin',
+    },
+  },
 }
 
 export const HEADER_MENU: Record<string, { name: string; link: string }>[] = [
@@ -121,4 +211,26 @@ export const HEADER_MENU: Record<string, { name: string; link: string }>[] = [
   ROUTES.hashtag,
   ROUTES.club,
   ROUTES.about,
+]
+
+export const FOOTER_MENU: Record<string, { name: string; link: string }>[] = [
+  ROUTES.announcement,
+  ROUTES.news,
+  ROUTES.event,
+  ROUTES.competition,
+  ROUTES.hashtag,
+]
+
+export const SUPPORT_MENU: Record<string, { name: string; link: string }>[] = [
+  ROUTES.helpCenter,
+  ROUTES.termsOfService,
+  ROUTES.legal,
+  ROUTES.privacyPolicy,
+  ROUTES.status,
+]
+
+export const SOCIETY_MENU: Record<string, { name: string; link: string }>[] = [
+  ROUTES.club,
+  ROUTES.about,
+  ROUTES.contactUs,
 ]
