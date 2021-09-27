911107521bebd1e6dd20e10c33176f3b82275169
yuskara <57836403+yuskara@users.noreply.github.com>
2021-09-27 05:51:25 -0400

    Add files via upload

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,1 +1,45 @@
+import { Box, Flex, Button, Text } from '@chakra-ui/react'
 
+export const Hero = (): JSX.Element => {
+  return (
+    <Flex
+      pos="relative"
+      bgSize="cover"
+      height="100vh"
+      direction="column"
+      bg={'white'}
+      marginTop="-100"
+      justify="center"
+      align="center"
+    >
+      <Box
+        as="video"
+        top={0}
+        left={0}
+        width="100%"
+        objectFit="cover"
+        autoPlay
+        loop
+        position="absolute"
+      >
+        <source
+          src="/images/Alley_hero_aug_2020-transcode.webm"
+          type="video/webm"
+        />
+      </Box>
+      <Text
+        position="relative"
+        textAlign="center"
+        fontSize="6xl"
+        textColor="black"
+        as="em"
+      >
+        Elemi gitti Lezzeti kaldi,
+        <p>Her dogan gun bir umuttur bize.</p>
+      </Text>
+      <Button size="lg" marginTop="400px" border="2px" borderColor="green.500">
+        Faaliyetlerimiz
+      </Button>
+    </Flex>
+  )
+}
