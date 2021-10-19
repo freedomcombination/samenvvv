ad107aedea114e05f5a2e2417881146a6538fab6
yuskara <ykaraca@gmail.com>
2021-10-19 16:20:16 -0400

    CardView added in MainView

---
+++ src/components/CardGroup/index.tsx
@@ -0,0 +1,17 @@
+import { ChakraProps, SimpleGrid } from '@chakra-ui/react'
+import { Card } from '@components'
+
+interface CardGroupProps extends ChakraProps {
+  items: SubpageType[]
+  isSimple: boolean
+}
+
+export const CardGroup = ({ items, isSimple }: CardGroupProps): JSX.Element => {
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
+      {items.map((item, i) => (
+        <Card key={i} item={item} isSimple={isSimple} />
+      ))}
+    </SimpleGrid>
+  )
+}
