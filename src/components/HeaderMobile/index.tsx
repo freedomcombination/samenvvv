01394960f56aee76b0a9d416041bdcd57c997291
Talip Altas <talipaltas@gmail.com>
2021-10-23 14:00:41 +0200

    Main Page & Sub Page (#57)
    * feat: added header mobile component (#53)
    
    * CardView added in MainView
    
    * Delete .env
    
    * Delete .gitignore
    
    * Delete package-lock.json
    
    * Update index.tsx
    
    * Delete yarn.lock
    
    * Hero Variant (#55)
    
    * style: redefined the height value of Hero using a ternary operator
    
    * fix: adjustment on the styling of some of the elements in small variant
    
    * feat: general design of the subview page (#56)
    
    * feat: general design of the subview page
    
    * fix: lint & share button size
    
    Co-authored-by: 7alip <talipaltas@gmail.com>
    
    Co-authored-by: yuskara <ykaraca@gmail.com>
    Co-authored-by: yuskara <57836403+yuskara@users.noreply.github.com>
    Co-authored-by: ismkl <69303744+ismkl@users.noreply.github.com>

---
+++ src/components/HeaderMobile/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  IconButton,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { FaBars } from 'react-icons/fa'
+
+import { HeaderNav, HeaderTop } from '@components'
+
+type HeaderMobileProps = {
+  hasScroll?: boolean
+  isScrolled?: boolean
+}
+
+export const HeaderMobile = (props: HeaderMobileProps) => {
+  const { isOpen, onToggle, onClose } = useDisclosure()
+  const { hasScroll, isScrolled } = props
+
+  return (
+    <>
+      <IconButton
+        display={{ base: 'flex', lg: 'none' }}
+        variant="outline"
+        colorScheme={
+          !hasScroll ? 'blackAlpha' : isScrolled ? 'blackAlpha' : 'whiteAlpha'
+        }
+        onClick={onToggle}
+        aria-label="menu"
+        icon={<FaBars />}
+      />
+      <Drawer isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader>Menu</DrawerHeader>
+          <DrawerBody>
+            <HeaderNav direction="column" />
+          </DrawerBody>
+          <DrawerFooter justifyContent="center">
+            <HeaderTop />
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -9,6 +9,7 @@ interface HeroProps {
   image?: string | ImageResponseType
   buttonText?: string
   link?: string
+  isFullHeight?: boolean
 }
 
 export const Hero = ({
@@ -18,9 +19,14 @@ export const Hero = ({
   image,
   buttonText,
   link,
+  isFullHeight = true,
 }: HeroProps): JSX.Element => {
   return (
-    <Box pos="relative" height="100vh" marginTop="-100px">
+    <Box
+      pos="relative"
+      height={isFullHeight ? '100vh' : '300px'}
+      marginTop={{ base: '-64px', lg: '-100px' }}
+    >
       {video && (
         <Box
           as="video"
@@ -60,14 +66,35 @@ export const Hero = ({
           mx="auto"
           textAlign="center"
         >
-          <Heading color="white" fontSize="6xl">
+          <Heading
+            color="white"
+            fontSize="6xl"
+            {...(!isFullHeight && {
+              pos: 'absolute',
+              bottom: 12,
+              left: '50%',
+              transform: 'translateX(-50%)',
+              w: 'full',
+            })}
+          >
             {title}
           </Heading>
-          <Text color="white" fontSize="lg" fontWeight="medium">
+          <Text
+            color="white"
+            fontSize="lg"
+            fontWeight="medium"
+            display={isFullHeight ? 'inherit' : 'none'}
+          >
             {description}
           </Text>
           {buttonText && link && (
-            <Navigate href={link} as={Button} size="lg" colorScheme="primary">
+            <Navigate
+              href={link}
+              as={Button}
+              size="lg"
+              colorScheme="primary"
+              display={isFullHeight ? 'inherit' : 'none'}
+            >
               {buttonText}
             </Navigate>
           )}
