1c1cb1ba340e51408115fe47bd3a87c9bba35b6d
Talip Altas <talipaltas@gmail.com>
2021-11-07 23:48:10 +0100

    feat(post-maker): search mention (#63)

--- src/components/MentionList/index.tsx
+++ src/components/MentionList/index.tsx
@@ -1,7 +1,16 @@
-import { useMemo } from 'react'
+import { useEffect, useState } from 'react'
 
-import { Avatar, Box, Button, HStack, Text, VStack } from '@chakra-ui/react'
+import {
+  Avatar,
+  Box,
+  Button,
+  HStack,
+  Text,
+  usePrevious,
+  VStack,
+} from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
+import { useQuery } from 'react-query'
 
 import { MentionListSkeleton } from '@components'
 import {
@@ -13,21 +22,80 @@ import {
 import { useMentionList } from 'src/lib/getData/getMentionList'
 
 export const MentionList = (): JSX.Element => {
-  const { data: mentionList, isLoading } = useMentionList()
-  const { mentions, postContent } = useAppSelector(state => state.postShare)
-
+  const [mentionListState, setMentionListState] = useState<IMention[]>([])
+  const { mentions, postContent, mentionSearchKey } = useAppSelector(
+    state => state.postShare,
+  )
   const dispatch = useAppDispatch()
   const { t } = useTranslation()
+  const prevSearchKey = usePrevious(mentionSearchKey)
+
+  const {
+    data: mentionList,
+    isLoading,
+    isFetched,
+    isError,
+    isPreviousData,
+  } = useMentionList()
+
+  // Query hook won't be triggered automatically
+  // It's supposed to fetch data from twitter
+  // if user can't find any mention from our db
+  const {
+    isLoading: isTwitterFetchLoading,
+    data: mentionListFromTwitter,
+    refetch,
+  } = useQuery<IMention[]>(
+    'search-mentions',
+    async () => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_ADMIN_URL}/mentions/search?username=${mentionSearchKey}`,
+      )
+      const rawData = await response.json()
+      return rawData.map((user: ITweetUserData) => ({
+        username: user.screen_name,
+        user_data: user,
+      }))
+    },
+    {
+      enabled: false,
+    },
+  )
+
+  useEffect(() => {
+    if (mentionSearchKey.length > 1) {
+      const filteredData =
+        mentionList?.filter(m =>
+          m.username.toLowerCase().includes(mentionSearchKey.toLowerCase()),
+        ) ?? []
+
+      if (filteredData.length === 0) {
+        refetch()
+      }
+
+      setMentionListState(filteredData as IMention[])
+    }
+
+    if (prevSearchKey && mentionSearchKey.length < 2) {
+      setMentionListState(mentionList as IMention[])
+    }
+  }, [mentionSearchKey, mentionList, prevSearchKey, refetch])
+
+  // Ony at the first time fill the state with fetched tada
+  useEffect(() => {
+    if (!isPreviousData && isFetched && mentionList)
+      setMentionListState(mentionList as IMention[])
+  }, [isFetched, mentionList, isPreviousData])
 
   const onAddMention = (mention: string) => {
     dispatch(addMention(mention))
     dispatch(checkCharacterCount(postContent))
   }
 
-  const currentMentionList = useMemo(
-    () => mentionList?.filter(m => !mentions.includes(m.username)),
-    [mentionList, mentions],
-  )
+  const currentMentionList = [
+    ...mentionListState,
+    ...(isError ? [] : mentionListFromTwitter ?? []),
+  ]?.filter(m => !mentions.includes(m.username))
 
   return (
     <>
@@ -45,7 +113,7 @@ export const MentionList = (): JSX.Element => {
         bg="white"
         overflowY="auto"
       >
-        {isLoading ? (
+        {isLoading || isTwitterFetchLoading ? (
           <MentionListSkeleton />
         ) : (
           currentMentionList?.map(({ username, user_data }, i) => (
