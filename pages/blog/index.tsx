a80461708a3f731b8253d5bf51e82c1982aeb305
7alip <talipaltas@gmail.com>
2021-12-29 21:59:45 +0100

    fix: show empty page if no posts found

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from '@chakra-ui/react'
+import { Image, SimpleGrid, Stack, Text } from '@chakra-ui/react'
 import { GetStaticProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { NextSeoProps } from 'next-seo'
@@ -13,18 +13,29 @@ interface BlogProps {
 
 // TODO: Implement author filter
 const Blog = ({ seo }: BlogProps): JSX.Element => {
-  const { data, isLoading } = useBlogPosts()
+  const { data, isLoading, isFetched } = useBlogPosts()
+
+  const hasData = isFetched && data && data.length > 0
 
   return (
-    <Layout seo={seo} scrollHeight={100} isLoading={isLoading}>
-      <Hero title="Blog" isFullHeight={false} />
-      <Container maxW="container.lg">
-        <SimpleGrid gap={8} py={8} columns={{ base: 1, lg: 2 }}>
-          {data?.map((post, index) => (
-            <BlogCard key={index} isFeatured={index === 0} post={post} />
-          ))}
-        </SimpleGrid>
-      </Container>
+    <Layout seo={seo} scrollHeight={hasData ? 100 : null} isLoading={isLoading}>
+      {hasData ? (
+        <Container maxW="container.lg">
+          <Hero title="Blog" isFullHeight={false} />
+          <SimpleGrid gap={8} py={8} columns={{ base: 1, lg: 2 }}>
+            {data?.map((post, index) => (
+              <BlogCard key={index} isFeatured={index === 0} post={post} />
+            ))}
+          </SimpleGrid>
+        </Container>
+      ) : (
+        <Stack minH="inherit" justify="center" align="center" spacing={8}>
+          <Image h={200} src="/images/no-blog.svg" alt="no blog" />
+          <Text textAlign="center" fontSize="lg">
+            Sorry! No articles published in this language.
+          </Text>
+        </Stack>
+      )}
     </Layout>
   )
 }
@@ -39,8 +50,6 @@ export const getStaticProps: GetStaticProps = async context => {
   const queryFn = () => getBlogPosts(locale as string)
 
   await queryClient.prefetchQuery(queryKey, queryFn)
-
-  const posts = queryClient.getQueryData<IPost[]>(queryKey)
 
   const blogSeo: Record<string, NextSeoProps> = {
     en: {
@@ -65,6 +74,5 @@ export const getStaticProps: GetStaticProps = async context => {
       dehydratedState: dehydrate(queryClient),
       ...(await serverSideTranslations(locale as string, ['common'])),
     },
-    notFound: !posts || posts?.length === 0,
   }
 }
